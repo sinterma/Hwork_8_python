9221bc38f3de1896c7094bd15672cdf16c76326f
Fedotovskaia Kseniia <sinterma@gmail.com>
2024-08-29 15:26:00 +0200

    new tests

--- conftest.py
+++ conftest.py
@@ -6,6 +6,9 @@ from models import Product, Cart
 def product():
     return Product("book", 100, "This is a book", 1000)
 
+@pytest.fixture
+def product2():
+    return Product("notebook", 150, "This is a notebook", 500)
 
 @pytest.fixture
 def cart():
